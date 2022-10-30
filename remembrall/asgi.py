5fffffa9aec3b41eb74e55eaba3cc62d827498b4
Furkan Durul <furkandurul@fb.com>
2022-10-31 01:05:08 +0300

    refactor: rename the project folder suitable with project name

--- remembrall/asgi.py
+++ remembrall/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Remembrall project.
+ASGI config for remembrall project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "remembrall.settings")
 
 application = get_asgi_application()
--- remembrall/wsgi.py
+++ remembrall/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Remembrall project.
+WSGI config for remembrall project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "remembrall.settings")
 
 application = get_wsgi_application()
