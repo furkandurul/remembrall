56719ff9358d877dee20caaefa720730ad8d2947
Furkan Durul <furkandurul@fb.com>
2022-11-01 23:08:50 +0300

    fix: update project related source code

--- remembrall_website/urls.py
+++ remembrall_website/urls.py
@@ -1,4 +1,4 @@
-"""remembrall URL Configuration
+"""remembrall_website URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
