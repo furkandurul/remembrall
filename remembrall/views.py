53aa4ca3b3c3ad218bd645262d3cd54fd35b8a2b
Furkan <furkan.durul@boun.edu.tr>
2022-11-06 03:15:31 +0300

    feat: create basic app without functionality (#22)
    create templates for future use
    update views for future use
    update timezone setting
    update user_id and post_id as int
    register app for the admin panel

--- remembrall/views.py
+++ remembrall/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
+
+from .models import User, Post
 
 def index(request):
 	return HttpResponse("Hello, world. You're at the polls index")
+
+def post(request, post_id):
+	template = loader.get_template('remembrall/post.html')
+	context = {}
+	return HttpResponse(template.render(context, request))
+
+def user_space(request, user_id):
+	#user_post_list = Post.objects.filter(creator_id=user_id)
+	template = loader.get_template('remembrall/user_space.html')
+	context = {}
+	return HttpResponse(template.render(context, request))
